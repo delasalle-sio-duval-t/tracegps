4762174a679db294da2456529aa45cafa9c62477
delasalle-sio-decastro-r <decastro.r@lyceedelasalle.fr>
2025-10-10 10:48:17 +0200

    Nouvelles fonctions

---
+++ modele/DAO.test1.php
@@ -0,0 +1,60 @@
+<?php
+// Projet TraceGPS
+// fichier : modele/DAO.test1.php
+// Rôle : test de la classe DAO.php
+// Dernière mise à jour : xxxxxxxxxxxxxxxxx par xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+// Le code des tests restant à développer va être réparti entre les membres de l'équipe de développement.
+// Afin de limiter les conflits avec GitHub, il est décidé d'attribuer un fichier de test à chaque développeur.
+// Développeur 1 : fichier DAO.test1.php
+// Développeur 2 : fichier DAO.test2.php
+// Développeur 3 : fichier DAO.test3.php
+// Développeur 4 : fichier DAO.test4.php
+
+// Quelques conseils pour le travail collaboratif :
+// avant d'attaquer un cycle de développement (début de séance, nouvelle méthode, ...), faites un Pull pour récupérer
+// la dernière version du fichier.
+// Après avoir testé et validé une méthode, faites un commit et un push pour transmettre cette version aux autres développeurs.
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Test de la classe DAO</title>
+	<style type="text/css">body {font-family: Arial, Helvetica, sans-serif; font-size: small;}</style>
+</head>
+<body>
+
+<?php
+// connexion du serveur web à la base MySQL
+include_once ('DAO.php');
+$dao = new DAO();
+
+
+// test de la méthode existeAdrMailUtilisateur ----------------------------------------------------
+// modifié par dP le 12/8/2021
+echo "<h3>Test de existeAdrMailUtilisateur : </h3>";
+if ($dao->existeAdrMailUtilisateur("admin@gmail.com")) $existe = "oui"; else $existe = "non";
+echo "<p>Existence de l'utilisateur 'admin@gmail.com' : <b>" . $existe . "</b><br>";
+if ($dao->existeAdrMailUtilisateur("delasalle.sio.eleves@gmail.com")) $existe = "oui"; else $existe = "non";
+echo "Existence de l'utilisateur 'delasalle.sio.eleves@gmail.com' : <b>" . $existe . "</b></br>";
+
+// test de la méthode getLesPointsDeTrace ---------------------------------------------------------
+// modifié par dP le 13/8/2021
+echo "<h3>Test de getLesPointsDeTrace : </h3>";
+$lesPoints = $dao->getLesPointsDeTrace(1);
+$nbPoints = sizeof($lesPoints);
+echo "<p>Nombre de points de la trace 1 : " . $nbPoints . "</p>";
+// affichage des points
+foreach ($lesPoints as $unPoint)
+{ echo ($unPoint->toString());
+    echo ('<br>');
+}
+
+
+// ferme la connexion à MySQL :
+unset($dao);
+?>
+
+</body>
+</html>
