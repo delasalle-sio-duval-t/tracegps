72693269519433859f92767160324569ad15a402
delasalle-sio-gronkiew-k <gronkiew.k@lyceedelasalle.fr>
2025-11-04 16:18:04 +0100

    ajout après merge

--- api/services/DemanderUneAutorisation.php
+++ api/services/DemanderUneAutorisation.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once ('C:\wamp64\www\ws-php-RD\TRACEGPS\modele\DAO.php');
+include_once ('C:\wamp64\www\ws-php-kg\TraceGPS\modele\DAO.php');
 
 global $ADR_MAIL_EMETTEUR, $ADR_SERVICE_WEB;
 
